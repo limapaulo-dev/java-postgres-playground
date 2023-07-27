f290c61442875d0ad560a60a83852a8fc1673cec
Paulo Lima <limapaulo.dev@gmail.com>
2023-07-27 14:22:54 +0000

    types & arrays

---
+++ src/main/java/com/example/Array.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import java.util.Arrays;
+
+public class Array {
+    public static void main(String[] args) {
+
+    int intArray[] = {10, 50, 78};
+        
+    System.out.println(Arrays.toString(intArray));
+    System.out.println(intArray[0]);
+    System.out.println(intArray[1]);
+    System.out.println(intArray[2]);
+
+    }
+}
---
+++ src/main/java/com/example/Types.java
@@ -0,0 +1,28 @@
+package com.example;
+
+public class Types {
+    public static void main(String[] args) {
+
+        byte byteNum = 15;
+        short shortNum = 15152;
+        int intNum = 897985152;
+        long longNum = 1546498798;
+
+        float floatNum = 15.15f;
+        double doubleNum = 415.554654654d;
+
+        int minhaIdade = 35;
+        char meuSexo = 'M';
+        String meuNome = "Paulo Lima";
+        boolean onlineConection = true;
+
+        String onlineCheck = (onlineConection) ? "Sim" : "Não";
+      
+        System.out.println("\n-----------------------------");
+        System.out.println("Meu nome é: " + meuNome);
+        System.out.println("Minha Idade: "+ minhaIdade);
+        System.out.println("Meu sexo é: "+ meuSexo);
+        System.out.println("Estamos Online? "+ onlineCheck);
+        System.out.println("-----------------------------\n");
+    }
+}
