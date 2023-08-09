e6ccda993a5da4a0fde5123fbc2fe61cbc1cb445
Paulo Lima <limapaulo.dev@gmail.com>
2023-08-09 01:10:34 +0000

    refactor query

--- src/main/java/com/example/AppPostgresDB.java
+++ src/main/java/com/example/AppPostgresDB.java
@@ -2,6 +2,8 @@ package com.example;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -9,6 +11,10 @@ public class AppPostgresDB {
     private static final String JDBC_URL = "jdbc:postgresql://localhost/postgres";
     private static final String USERNAME = "gitpod";
     private static final String PASSWORD = "";
+
+    private Connection getConnection() throws SQLException{
+        return DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
+    }
 
     public static void main(String[] args) {
         new AppPostgresDB();
@@ -18,15 +24,67 @@ public class AppPostgresDB {
 
         try (var conn = getConnection()){
             System.out.println("DB conected");
+
+            findTable(conn, "produto");
             
-            listStates(conn);
+/*             listStates(conn);
             System.out.println("");
-            searchState(conn,"TO");
+            searchState(conn,"TO"); */
 
         } catch (SQLException e) {
             System.err.println("Não foi possível conectar ao DB");
             System.err.println(e.getMessage());
         }
+    }
+
+    private void findTable(Connection conn, String tableName) {
+        var sql = "select * from " + tableName;
+
+        try {
+            var statement = conn.createStatement();
+            var result = statement.executeQuery(sql);
+
+            travelTable(result);
+
+        } catch (SQLException e) {
+            System.err.println("Query Failed");
+            e.printStackTrace();
+        }
+    }
+
+    private void travelTable(ResultSet result) throws SQLException {
+        
+        var tableMeta = result.getMetaData();
+        String tableName = tableMeta.getTableName(1);
+        int cols = tableMeta.getColumnCount();
+
+        System.out.println();
+        printTableName(tableName);
+        System.out.println();
+
+        printColumnTitles(tableMeta, cols);
+        printColumnContent(result, cols);
+    }
+
+    private void printTableName(String tableName) {
+        System.out.println("Table name: " + tableName);
+    }
+
+    private void printColumnTitles(ResultSetMetaData tableMeta, int cols) throws SQLException {
+        for (int i = 1; i <= cols; i++) {
+            System.out.printf("%-25s | ", tableMeta.getColumnName(i));
+        }
+        System.out.println();
+    }  
+    
+    private void printColumnContent(ResultSet result, int cols) throws SQLException {
+        while (result.next()){
+            for (int i = 1; i <= cols; i++) {
+                System.out.printf("%-25s | ", result.getString(i));
+            } 
+            System.out.println();                
+        }
+        System.out.println();
     }
 
     private void searchState(Connection conn, String uf) {
@@ -64,18 +122,8 @@ public class AppPostgresDB {
             System.err.println(e.getMessage());
         };
     }
+
+
     
-    private Connection getConnection() throws SQLException{
-        return DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
-    }
 
-    private void loadDriverJDBC() {
-        try {
-            Class.forName("org.postgresql.Driver");
-
-        } catch (ClassNotFoundException e) {
-            System.err.println("Não foi possível carregar a biblioteca/classe para conectar ao DB");
-            System.err.println(e.getMessage());
-        };
-    }
 }
